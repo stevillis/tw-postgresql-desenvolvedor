9962dfde7e28d566b10867ebdf8e9e688b8faee3
Stévillis Sousa <stevillis@hotmail.com>
2022-01-18 13:21:50 -0400

    Select tuples

---
+++ _5ManipulandoInformacoesEmTabelas/_2ProjecoesComSelect.sql
@@ -0,0 +1,10 @@
+-- Selecionando todas as colunas
+SELECT *
+FROM geral.prd_produtos;
+
+
+-- Selecionando colunas específicas
+SELECT PRD_NOME,
+	PRD_QUANTIDADE_ESTOQUE,
+	PRD_UNIDADE
+FROM geral.prd_produtos;
