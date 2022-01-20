e95d8153335e8b539247d58ea6897bfebca69041
Stévillis Sousa <stevillis@hotmail.com>
2022-01-20 16:43:44 -0400

    Retrieve data from multiple tables with JOIN

---
+++ _7ObtendoDadosDeVariasTabelasClausulaJOIN/_2DadosDeMultiplasTabelsComJOIN.sql
@@ -0,0 +1,12 @@
+SELECT nfe.NFE_ID,
+	nfe.NFE_DATA_EMISSAO,
+	nfe.NFE_CHAVE_CONSULTA,
+	nfe.NFE_STATUS,
+	prd.PRD_NOME,
+	nit.NIT_QUANTIDADE,
+	prd.PRD_UNIDADE,
+	cli.CLI_NOME
+FROM geral.CLI_CLIENTES AS cli 
+	JOIN financeiro.NFE_NOTAS_FISCAIS AS nfe ON cli.CLI_ID = nfe.CLI_ID
+	JOIN financeiro.NIT_ITENS_NOTA_FISCAL AS nit ON nfe.NFE_ID = nit.NFE_ID
+	JOIN geral.PRD_PRODUTOS AS prd ON nit.PRD_ID = prd.PRD_ID;
